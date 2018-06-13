5bce3ccaa43fe14087d2044a3c9696f9a8b1e0c1
alvations <alvations@gmail.com>
2018-06-13 16:08:57 +0800

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'komorebi',
   packages = ['komorebi'],
-  version = '0.0.2',
+  version = '0.0.3',
   description = 'Parallel Data API',
   author = 'Liling Tan',
   license = 'MIT',
