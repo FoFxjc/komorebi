2f9bd8cb8fcec88b9d3e846684825e738d5e7aa4
alvations <alvations@gmail.com>
2018-10-18 17:05:17 +0800

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'komorebi',
   packages = ['komorebi'],
-  version = '0.0.6',
+  version = '0.0.7',
   description = 'Text Data API',
   author = 'Liling Tan',
   license = 'MIT',
