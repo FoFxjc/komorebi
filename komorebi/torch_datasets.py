dfd805d5cb3b15cc4a2aa9ada4e0e2987e94b243
alvations <alvations@gmail.com>
2018-10-18 17:04:47 +0800

    added torch thingy

--- komorebi/torch_datasets.py
+++ komorebi/torch_datasets.py
@@ -1,37 +1,52 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 import torch
-from torch import LongTensor
-from torch.autograd import Variable
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
 
 from komorebi.parallel import ParallelData
 
+class ParallelDataset(Dataset):
+    def __init__(self, src_file, trg_file, max_len):
+        # ParallelData object is an iterator packe with goodies =)
+        self._data = ParallelData(src_file, trg_file)
+        # Iterate through the dataset to get the source and data.
+        _source_texts, _target_texts = zip(*self._data)
+        self._data.reset() # Resets the iterator to the start.
+        # Pre-computes the no. of data points.
+        self._len = len(_source_texts)
+        self.source_lens = np.clip(list(map(len, _source_texts)), 1, max_len)
 
-class TorchParallelData(ParallelData):
-    def __init__(self, *args, **kwargs):
-        super(TorchParallelData, self).__init__(*args, **kwargs)
-        # PyTorch nonsense with CUDA... -_-|||
-        if 'use_cuda' in kwargs:
-            self.use_cuda = kwargs['use_cuda']
-        else:
-            self.use_cuda = torch.cuda.is_available()
+        # Converts the indices to torch tensors.
+        pad_idx = self._data.src_data.PAD_IDX
+        self.source_texts = np.array([self.pad_sequence(_s, max_len, pad_idx)
+                                      for _s in _source_texts])
+        self.target_texts = np.array([self.pad_sequence(_t, max_len, pad_idx)
+                                      for _t in _target_texts])
 
-    def create_variable(self, array_1d):
-        """
-        Create the PyTorch variable given a sentence
+    def pad_sequence(self, sequence, max_len, pad_idx):
+        padded_sequence = np.zeros(max_len, dtype=np.int64)
+        padded_sequence[:len(sequence)] = sequence[:max_len]
+        if pad_idx != 0:
+            padded_sequence[len(x):] = pad_idx
+        return padded_sequence
 
-        :param array_1d: Input 1D array of vocab indices.
-        :type array_1d: list(int)
-        """
-        result = Variable(LongTensor(array_1d).view(-1, 1))
-        return result.cuda() if self.use_cuda else result
+    def __len__(self):
+        return self._len
 
-    def _iterate(self):
-        """
-        The helper function to iterate through the source and target file
-        and convert the lines into vocabulary indices.
-        """
-        for src_line, trg_line in zip(self.src_data.lines(), self.trg_data.lines()):
-            src_sent = self.create_variable(self.src_data.variable_from_sent(src_line, self.src_data.vocab))
-            trg_sent = self.create_variable(self.trg_data.variable_from_sent(trg_line, self.trg_data.vocab))
-            yield src_sent, trg_sent
+    def __getitem__(self, index):
+        return {'x': self.source_texts[index],
+                'x_len': self.source_lens[index],
+                'y': self.target_texts[index]}
+
+    def batches(self, batch_size, shuffle=True):
+        dataloader = DataLoader(dataset=self, batch_size=batch_size, shuffle=shuffle)
+
+        for data_dict in dataloader:
+            # Sort indices of data in batch by lengths.
+            sorted__indices = np.array(data_dict['x_len']).argsort()[::-1].tolist()
+            data_batch = {name:_tensor[sorted__indices]
+                          for name, _tensor in data_dict.items()}
+            yield data_batch
